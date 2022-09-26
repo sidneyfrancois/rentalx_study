d22dda500f6d4c23f719d63b41ee98bff1a07578
Sidney François <sidney.fvm.ps@outlook.com>
2022-09-25 22:15:29 -0300

    II-4-Criando use case de especificação

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,21 +1,13 @@
 import { Router } from "express";
 import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const specificationsRoutes = Router();
 
 const specificationsRepository = new SpecificationsRepository();
 
 specificationsRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  const createSpecificationService = new CreateSpecificationService(
-    specificationsRepository
-  );
-
-  createSpecificationService.execute({ name, description });
-
-  return response.status(201).send();
+  return createSpecificationController.handle(request, response);
 });
 
 specificationsRoutes.get("/", (request, response) => {
