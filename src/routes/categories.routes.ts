19c0e8c394fcb7b4c18f34936e8343c42bcc6e5d
Sidney François <sidney.fvm.ps@outlook.com>
2022-09-26 18:15:46 -0300

    III-3-Conhecendo o TSyringe

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,14 +1,13 @@
 import { Router } from "express";
 import { CategoriesRepository } from "../modules/cars/repositories/implementations/CategoriesRepository";
-import createCategoryController from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post("/", (request, response) => {
-  return createCategoryController().handle(request, response);
-});
+const createCategoryController = new CreateCategoryController();
+
+categoriesRoutes.post("/", createCategoryController.handle);
 
 categoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
+import "reflect-metadata";
 import express from "express";
 import { router } from "./routes";
 import { createConnection } from "./database";
+import "./shared/container";
 
 createConnection();
 
