2d1a6c6481584b87c8f00cb925eb84f419673f15
Sidney François <sidney.fvm.ps@outlook.com>
2022-10-05 18:47:37 -0300

    IV-3-Criando middleware de administrador

---
+++ src/shared/infra/http/middlewares/ensureAdmin.ts
@@ -0,0 +1,20 @@
+import { UserRepository } from "../../../../modules/accounts/infra/typeorm/repositories/UserRepository";
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "../../../errors/AppError";
+
+export async function ensureAdmin(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const { id } = request.user;
+
+  const userRepository = new UserRepository();
+  const user = await userRepository.findById(id);
+
+  if (!user.isAdmin) {
+    throw new AppError("User is not admin!");
+  }
+
+  return next();
+}
--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -1,13 +1,20 @@
 import { Router } from "express";
 import { CreateCategoryController } from "../../../../modules/cars/useCases/createCategory/CreateCategoryController";
 import { ListCategoriesController } from "../../../../modules/cars/useCases/listCategories/ListCategoriesController";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const categoriesRoutes = Router();
 const createCategoryController = new CreateCategoryController();
 const listCategoriesController = new ListCategoriesController();
 
-categoriesRoutes.post("/", createCategoryController.handle);
+categoriesRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createCategoryController.handle
+);
 
-categoriesRoutes.get("/", listCategoriesController.handle);
+categoriesRoutes.get("/", ensureAuthenticated, listCategoriesController.handle);
 
 export { categoriesRoutes };
